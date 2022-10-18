5fbc00db9a7e8c868fceb248652446a1ee166302
LAPTOP-8GDS7GCB\jiny7 <jiny7157502@naver.com>
2022-10-18 16:30:38 +0900

    기능 수정 커밋

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import add as a
+import sub as s
+
 num1, num2 = 100, 200
-print(num1+num2)
+
+add_result = a.add(num1, num2)
+sub_result = s.sub(num1, num2)
+
+print(add_result)
+print(sub_result)
